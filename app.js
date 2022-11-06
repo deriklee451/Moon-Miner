8d758e6c577cb93af4bd2a9c0e023c3e87a325d2
derik banning <deriklee451@gmail.com>
2022-11-06 09:04:12 -0700

    updates

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ let clickUpgrades = [
         price: 100,
         quantity: 0,
         multiplier: 1
+    },
+    {
+        name: 'shovel',
+        price: 200,
+        quantity: 0,
+        multiplier: 3
     }
 ]
 
@@ -17,7 +23,13 @@ let automaticUpgrades = [
         name: 'rover',
         price: 600,
         quantity: 0,
-        multiplier: 20,
+        multiplier: 20
+    },
+    {
+        name: 'mining rig',
+        price: 1000,
+        quantity: 0,
+        multiplier: 1000
     }
 ]
 
@@ -37,6 +49,7 @@ function mine() {
     update()
 }
 
+// NOTE Clickable upgrades
 
 function buyPickaxe() {
     let pickaxe = clickUpgrades.find(c => c.name == 'pickaxe')
@@ -44,6 +57,7 @@ function buyPickaxe() {
         cheese -= pickaxe.price
         pickaxe.quantity++
         pickaxe.price = pickaxe.price * 2
+        document.getElementById('pick-cost').innerHTML = pickaxe.price
         console.log(pickaxe);
         update()
     }
@@ -52,12 +66,27 @@ function buyPickaxe() {
     else alert('need more cheese');
 }
 
+function buyShovel() {
+    let shovel = clickUpgrades.find(c => c.name == 'shovel')
+    if (cheese >= shovel.price) {
+        cheese -= shovel.price
+        shovel.quantity++
+        shovel.price = shovel.price * 2
+        update()
+    }
 
+    else alert('Not enough cheese')
+}
+
+
+
+// NOTE UPDATE
 function update() {
     document.getElementById('score').innerText = cheese
 }
 
 
+// NOTE Automatic upgrades
 function buyRover() {
     let rover = automaticUpgrades.find(a => a.name == 'rover')
     if (cheese >= rover.price) {
@@ -69,6 +98,18 @@ function buyRover() {
     else alert('You need more cheese')
 
 }
+
+function buyRig() {
+    let rig = automaticUpgrades.find(r => r.name == 'mining rig')
+    if (cheese >= rig.price) {
+        cheese -= rig.price
+        rig.quantity++
+        rig.price = rig.price * 2
+        update()
+    }
+    else alert('You need more cheese')
+}
+
 
 function collectAutoUpgrades() {
     automaticUpgrades.forEach(a => {
